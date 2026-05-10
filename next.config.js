9a143255275948b057bdc75c0b8606375075632a
Jack Harrison <jack@palateandpen.co.uk>
2026-05-10 19:01:05 +0100

    Fix subdomain routing loop

--- next.config.js
+++ next.config.js
@@ -5,14 +5,14 @@ const nextConfig = {
     return {
       beforeFiles: [
         {
-          source: '/:path*',
-          has: [{ type: 'host', value: 'app.palateandpen.co.uk' }],
-          destination: '/app/:path*',
-        },
-        {
           source: '/',
           has: [{ type: 'host', value: 'app.palateandpen.co.uk' }],
           destination: '/app',
+        },
+        {
+          source: '/:path((?!app|api|_next|favicon).+)',
+          has: [{ type: 'host', value: 'app.palateandpen.co.uk' }],
+          destination: '/app/:path',
         },
       ],
     };
